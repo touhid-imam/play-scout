2724094490cb4a88d8622ed1ca63c16914c2ab25
touhid-imam <touhid.imam1337@gmail.com>
2023-09-30 00:14:45 -0500

    selectedGenre highlight

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,11 +11,12 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-function GenreList({ onSelectGenre }: Props) {
+function GenreList({ selectedGenre, onSelectGenre }: Props) {
   const { data, isLoading, error } = useGenres();
-
+  console.log(selectedGenre);
   if (error) return null;
 
   if (isLoading) return <Spinner />;
@@ -33,6 +34,7 @@ function GenreList({ onSelectGenre }: Props) {
               fontSize="lg"
               variant="link"
               onClick={() => onSelectGenre(genre)}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
             >
               {genre.name}
             </Button>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -12,7 +12,6 @@ const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
-    console.log(deps);
 
     useEffect(() => {
         const controller = new AbortController();
