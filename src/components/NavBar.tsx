359c218af0311526d895a257c9147f04680c3f5a
touhid-imam <touhid.imam1337@gmail.com>
2023-10-01 02:40:24 -0500

    search component added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 function NavBar() {
   return (
-    <HStack justifyContent="space-between" padding="15px">
+    <HStack padding="15px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
