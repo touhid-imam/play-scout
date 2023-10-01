2c3c10299c6dc12a774dc41b6c33b8326a768545
touhid-imam <touhid.imam1337@gmail.com>
2023-10-01 03:26:27 -0500

    dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -42,6 +43,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" padding="10px">
+        <GameHeading gameQuery={gameQuery} />
         <HStack marginBottom={4}>
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
