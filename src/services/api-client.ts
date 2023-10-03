73d595209f61650b381aab5e368ac67066c96143
touhid-imam <touhid.imam1337@gmail.com>
2023-10-03 02:26:13 -0500

    remove api comment and added favicon

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -6,10 +6,3 @@ export default axios.create({
         key: import.meta.env.VITE_RAWG_SECRET_KEY
     }
 });
-
-// export default axios.create({
-//     baseURL: 'https://api.rawg.io/api',
-//     params: {
-//         key: '63ce20df37dd4d00bc830bc6dc426ff7'
-//     }
-// });
