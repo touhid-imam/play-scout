acc74066371392ad5b3a6ad807505522e4f70001
touhid-imam <touhid.imam1337@gmail.com>
2023-12-26 00:14:38 -0600

    create reusable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
-// import { Genre } from "./useGenres";
+import APIClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string
-}
+const apiClient = new APIClient<Game>("/games")
 
 export interface Game {
     id: number;
@@ -24,15 +19,14 @@ const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>(
   queryKey: ['games', gameQuery],
   queryFn: () => 
     apiClient
-      .get<FetchResponse<Game>>('/games', {
+      .getAll({
         params: {
-            genres: gameQuery.genre?.id, 
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText
-          }
+          genres: gameQuery.genre?.id, 
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText
+        }
       })
-      .then(res => res.data)
 })
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,10 @@
 // import useData from "./useData";
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import apiClient from "../services/api-client";
+import APIClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
+
+const apiClient = new APIClient<Genre>("/genres")
 
 export interface Genre{
     id: number;
@@ -14,10 +16,7 @@ export interface Genre{
 // const useGenres = () => ({data: genres, isLoading: false, error: null});
 const useGenres = () => useQuery({
     queryKey: ['genres'],
-    queryFn: () => 
-        apiClient
-            .get<FetchResponse<Genre>>('/genres')
-            .then(res => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hour
     initialData: {count: genres.length, results: genres}
 });
