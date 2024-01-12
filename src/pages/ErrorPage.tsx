d55fe6a84324ec38c312ab442508cb6ffbfd768a
touhid-imam <touhid.imam1337@gmail.com>
2024-01-12 12:35:20 -0600

    Handle-Error

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar />
+      <Flex flexDirection="column" alignItems="center" pt="60px">
+        <Heading>Oops!</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page does not exist."
+            : "An unexpected error occurred."}
+        </Text>
+      </Flex>
+    </>
+  );
+};
+
+export default ErrorPage;
