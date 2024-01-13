36459a9ef32549a1c9cad1d3bea97cac0160d4ee
touhid-imam <touhid.imam1337@gmail.com>
2024-01-12 18:51:14 -0600

    build exapandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,35 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+  const limit = 300;
+
+  if (!children) return null;
+
+  if (children.length <= 300) {
+    return <Text>{children}</Text>;
+  }
+
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+
+  return (
+    <div>
+      {summary}
+      <Button
+        size="xs"
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Read More" : "Show Less"}
+      </Button>
+    </div>
+  );
+};
+
+export default ExpandableText;
